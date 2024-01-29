3d30c8012492ac73e676ff554070cdf351f0564d
FabiollaPatriota <fabiollapatriota2@gmail.com>
2024-01-29 00:18:06 -0300

    Initial commit

---
+++ src/projetoFinalAc.java
@@ -0,0 +1,9 @@
+public class projetoFinalAc {
+    public static void main(String[] args) {
+        // Criar instância da agenda
+        Agenda agenda = new Agenda();
+
+        // Usar o menu de operações
+        agenda.exibirMenu();
+    }
+}
